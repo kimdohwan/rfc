6e85f5b8661877491bee60da34792f17f8975bef
kimdohwan <39307213+kimdohwan@users.noreply.github.com>
2020-10-16 18:38:57 +0900

    post list

--- tmp_blog/views.py
+++ tmp_blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from tmp_blog.models import Post
+
+
+def index(request):
+    return render(request, 'tmp_blog/index.html', {})
+
 
 def post_list(request):
-    return render(request, 'tmp_blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'tmp_blog/post_list.html', {'posts': posts})
